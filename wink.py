676aa056b99280f1c3d450c65432e183d5134545
Cherkesky <guy@cherkesky.com>
2020-06-25 16:45:14 -0500

    both the delete and the post functions check todays date before taking any action

--- wink.py
+++ wink.py
@@ -1,13 +1,14 @@
-import requests, json
+import requests, json, datetime
 from secret.devices import listinglocks
 from secret.urls_wink import baseURL
 from secret.keys_wink import authToken, contentType
 from bnb import getTodaysCheckins
 
 
-dummydata = [{'name': 'Test', 'code': '1173', 'checkin': '2020-06-25',
+dummydata = [{'name': 'GuyCh', 'code': '1173', 'checkin': '2020-06-25',
     'checkout': '2020-06-26', 'locks': ['257145']}]
 
+today = str(datetime.datetime.now()).split(" ")[0]
 
 def makeWorkOrder():
     TodaysCheckins = getTodaysCheckins()
@@ -24,50 +25,55 @@ def makeWorkOrder():
 
 
 def programCodes(workorder):
-    print(workorder)
+    print ("Boop Boop - Codes Are Being Programmed...")
     for guest in workorder:
         locks = guest['locks']
-        for lock in locks:
-            try:
-              url = f"{baseURL}/locks/{lock}/keys"
-              guestCode = guest['code']
-              guestName = guest['name']
-              payload = f"{{\n    \"code\": \"{guestCode}\", \n    \"name\": \"{guestName}\"\n    }}"
-              headers = {
-              'Content-Type': contentType,
-              'Authorization': authToken
-              }
+        guestCode = guest['code']
+        guestName = guest['name']
+        guestCheckIn = guest['checkin']
+        if guestCheckIn==today:
+          for lock in locks:
+              try:
+                url = f"{baseURL}/locks/{lock}/keys"
+                payload = f"{{\n    \"code\": \"{guestCode}\", \n    \"name\": \"{guestName}\"\n    }}"
+                headers = {
+                'Content-Type': contentType,
+                'Authorization': authToken
+                }
 
-              requests.request("POST", url, headers=headers, data = payload)
+                requests.request("POST", url, headers=headers, data = payload)
+                print("------------------------------------------------------")
+                print ("POST request for: ", guestName)
+                print ("url: ", url)
+                print ("payload: ", payload)
+                print("------------------------------------------------------")
+                print("DONE")
 
-              print ("url: ", url)
-              print ("payload: ", payload)
-              print("------------------------------------------------------")
-            except Exception as e:
-              print(e)
+              except Exception as e:
+                print(e)
+
 
 def deleteCodes(workorder):
-  print(workorder)
+  print ("Boop Boop - Codes Are Being Deleted...")
   for guest in workorder:
     locks = guest['locks']
     for lock in locks:
-      print (lock)
+      print ("Lock: ", lock)
       url = f"{baseURL}/locks/{lock}/keys"
       guestName = guest['name']
+      guestCheckOut = guest['checkout']
       payload = {}
       headers = {
         'Content-Type': contentType,
         'Authorization': authToken
         }
+
       response = requests.request("GET", url, headers=headers, data = payload)
       utf8Response = response.text.encode('utf8')
       jsonResponse = json.loads(utf8Response)
       keys = jsonResponse['data']
       for key in keys:
-        print ("name: ",key['name'])
-        print ("key_id: ", key['key_id'])
-        print("------------------------------------------------------")
-        if key['name'] == guestName:
+        if key['name'] == guestName and guestCheckOut==today:
           try:
             url = f"{baseURL}/keys/{key['key_id']}"
             headers = {
@@ -76,16 +82,23 @@ def deleteCodes(workorder):
             }
 
             requests.request("DELETE", url, headers=headers, data = payload)
-
+            print("------------------------------------------------------")
+            print ("DELETE request for: ", guestName)
+            print ("Checking Out At: ", guestCheckOut)
             print ("url: ", url)
             print ("payload: ", payload)
+            print ("name: ",key['name'])
+            print ("key_id: ", key['key_id'])
             print("------------------------------------------------------")
+            print("DONE")
+
           except Exception as e:
             print(e)
-
+        else: print (key['name'], " Not needed to be deleted today")
       
 
 # programCodes(makeWorkOrder())
+# deleteCodes(makeWorkOrder())
+
 # programCodes(dummydata)
 deleteCodes(dummydata)
-# deleteCodes(makeWorkOrder())
