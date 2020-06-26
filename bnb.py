e31166ce55659f1582e3138a5dc45a625a2f6278
Cherkesky <guy@cherkesky.com>
2020-06-26 12:57:43 -0500

    auto obtaining refresh token. updated the resource file and reloading it

--- bnb.py
+++ bnb.py
@@ -1,7 +1,9 @@
 import requests
 import datetime
 import json
-from secret.keys_bnb import authToken, contentType
+import re
+import importlib
+from secret import keys_bnb
 from secret.listings import airbnbAll
 from secret.urls_bnb import baseURL
 
@@ -11,28 +13,65 @@ def getTodaysCheckins():
 
   payload = {}
   headers = {
-    'Content-Type': contentType,
-    'Authorization': authToken
+    'Content-Type': keys_bnb.contentType,
+    'Authorization': keys_bnb.authToken
     }
-  response = requests.request("GET", url, headers=headers, data = payload)
+  try:
+    response = requests.request("GET", url, headers=headers, data = payload)
+  except Exception as e:
+    print(e)
 
   utf8Response = response.text.encode('utf8')
   jsonResponse = json.loads(utf8Response)
-  # print (utf8Response)
+  print (jsonResponse)
+  try:
+    if jsonResponse['status_code']==401:
+      print ("Token expired. Obtaining new Token")
+      url = "https://auth.smartbnb.io/oauth/token"
+      payload = 'audience=api.smartbnb.io&grant_type=client_credentials'
+      headers = {
+        'Authorization': keys_bnb.refreshToken,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+      response = requests.request("POST", url, headers=headers, data = payload)
+      
+      tokenResponseUtf8=(response.text.encode('utf8'))
+      jsonNewToken = json.loads(tokenResponseUtf8)
+      newRefreshedToken = jsonNewToken['access_token']
 
-  checkinDict ={}
-  checkinList =[]
+      print ("NEW TOKEN: ", newRefreshedToken)
+      try:
+        with open ('./secret/keys_bnb.py',"r+") as keyfile:
+          keyfileContent = keyfile.read()
+          keyfileContent = re.sub(f'authToken=\"(.*?)\"', f'authToken=\"Bearer {newRefreshedToken}\"', keyfileContent)
+          keyfile.seek(0)
+          keyfile.write (keyfileContent)
+          keyfile.truncate()
+          
+      except FileNotFoundError:
+        keyfileContent = None
+      print (keyfileContent)
+      importlib.reload(keys_bnb) # reloading the updated authToken resource
+      getTodaysCheckins() # Lets try again. Now with a new token.
 
-  for guest in jsonResponse:
-    if guest=="_pagination": 
-      break
-    checkinDict['listing'] = jsonResponse['data'][0]['listing_id']
-    checkinDict['checkin'] = jsonResponse['data'][0]['check_in'].split('T')[0]
-    checkinDict['checkout'] = jsonResponse['data'][0]['check_out'].split('T')[0]
-    checkinDict['name'] = jsonResponse['data'][0]['_included'][0]['data']['first_name']
-    checkinDict['code'] = jsonResponse['data'][0]['_included'][0]['data']['phone'].split('-')[1]
-    checkinList.append(checkinDict)
-    print (checkinList)
-  return (checkinList)
-# print (getTodaysCheckins())
+    else:
+      checkinDict ={}
+      checkinList =[]
+
+      for guest in jsonResponse:
+        if guest=="_pagination": 
+          break
+        checkinDict['listing'] = jsonResponse['data'][0]['listing_id']
+        checkinDict['checkin'] = jsonResponse['data'][0]['check_in'].split('T')[0]
+        checkinDict['checkout'] = jsonResponse['data'][0]['check_out'].split('T')[0]
+        checkinDict['name'] = jsonResponse['data'][0]['_included'][0]['data']['first_name']
+        checkinDict['code'] = jsonResponse['data'][0]['_included'][0]['data']['phone'].split('-')[1]
+        checkinList.append(checkinDict)
+        # print (checkinList)
+      return (checkinList)
+  
+  except Exception as e:
+    print(e) 
+
+print (getTodaysCheckins())
 
