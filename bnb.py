1b63d14af3dcc4f6f8898fdabcd2d59ab50090d7
Cherkesky <guy@cherkesky.com>
2020-06-28 00:28:40 -0500

    added extra protections from 401, 404 responses. seperated the refresh token logic to an independent func

--- bnb.py
+++ bnb.py
@@ -1,5 +1,5 @@
 import requests
-import datetime
+from datetime import datetime, timedelta
 import json
 import re
 import importlib
@@ -9,8 +9,10 @@ from secret.listings import airbnbAll
 from secret.urls_bnb import baseURL
 
 def getTodaysCheckins():
-  date = str(datetime.datetime.now()).split(" ")[0] #YYYY-MM-DD
-  url = f"{baseURL}?{airbnbAll}&start_date={date}&end_date={date}&include=guest"
+  today = str(datetime.now()).split(" ")[0]  # YYYY-MM-DD
+  startDate = str(datetime.now()-timedelta(days=10)).split(" ")[0]  # YYYY-MM-DD -10days
+
+  url = f"{baseURL}?{airbnbAll}&start_date={startDate}&end_date={today}&include=guest"
 
   payload = {}
   headers = {
@@ -18,60 +20,66 @@ def getTodaysCheckins():
     'Authorization': keys_bnb.authToken
     }
   try:
-    response = requests.request("GET", url, headers=headers, data = payload)
+    response = requests.request("GET", url, headers=headers, data=payload)
   except Exception as e:
     print(e)
-
   utf8Response = response.text.encode('utf8')
   jsonResponse = json.loads(utf8Response)
   try:
-    statusCode = 000 # initial value. might get changed by an error code
+    statusCode = 000  # initial value. might get changed by an error code
     try:
       statusCode = jsonResponse['status_code']
-      print ("Status Code: ", statusCode)
+      print("Status Code: ", statusCode)
     except:
-       print ("Seems like everything is ok")
-
-    if statusCode==401: # Unauthorized. Token expired.
-      print ("Token expired. Obtaining new Token")
-      url = "https://auth.smartbnb.io/oauth/token"
-      payload = 'audience=api.smartbnb.io&grant_type=client_credentials'
-      headers = {
-        'Authorization': keys_bnb.refreshToken,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-      response = requests.request("POST", url, headers=headers, data = payload)
-      tokenResponseUtf8=(response.text.encode('utf8'))
-      jsonNewToken = json.loads(tokenResponseUtf8)
-      newRefreshedToken = jsonNewToken['access_token']
-
-      # print ("NEW TOKEN: ", newRefreshedToken)
-      try: # Updating the authToken value in the resource file
-        with open ('./secret/keys_bnb.py',"r+") as keyfile:
-          keyfileContent = keyfile.read()
-          keyfileContent = re.sub(f'authToken=\"(.*?)\"', f'authToken=\"Bearer {newRefreshedToken}\"', keyfileContent)
-          keyfile.seek(0)
-          keyfile.write (keyfileContent)
-          keyfile.truncate()      
-      except FileNotFoundError:
-        keyfileContent = None
-      # print (keyfileContent)
-      importlib.reload(keys_bnb) # reloading the updated authToken resource
-      getTodaysCheckins() # Lets try again. Now with a new token.
-    else: # All good. Token is still valid
-      checkinDict ={}
+       print("Seems like everything is ok")
+    if statusCode == 404:
+      print ("No results found")
+      return 404
+    elif statusCode == 401:  # Unauthorized. Token expired. 
+      print("Token expired. Obtaining new Token")
+      getRefreshedToken()
+    else: # All good. Token is still valid and valid data returned
       checkinList =[]
-      for guest in jsonResponse:
-        if guest=="_pagination": # That's the irrelevant part
-          break
-        checkinDict['listing'] = jsonResponse['data'][0]['listing_id']
-        checkinDict['checkin'] = jsonResponse['data'][0]['check_in'].split('T')[0]
-        checkinDict['checkout'] = jsonResponse['data'][0]['check_out'].split('T')[0]
-        checkinDict['name'] = jsonResponse['data'][0]['_included'][0]['data']['first_name']
-        checkinDict['code'] = jsonResponse['data'][0]['_included'][0]['data']['phone'].split('-')[1]
-        checkinList.append(checkinDict)
-      return (checkinList)
+      for guest in jsonResponse['data']:
+        checkinDict ={}
+        if guest['status'] == "ACCEPTED":
+          checkinDict['listing'] = guest['listing_id']
+          checkinDict['checkin'] = guest['check_in'].split('T')[0]
+          checkinDict['checkout'] = guest['check_out'].split('T')[0]
+          checkinDict['name'] = guest['_included'][0]['data']['first_name']
+          checkinDict['code'] = guest['_included'][0]['data']['phone'].split('-')[1]
+          checkinList.append(checkinDict)
+      if len(checkinList) == 0:
+        return 404
+      else: 
+        print ("BNB Work Order: ",checkinList)
+        return checkinList
+
   except Exception as e:
     print(e) 
 
+
+def getRefreshedToken():
+  url = "https://auth.smartbnb.io/oauth/token"
+  payload = 'audience=api.smartbnb.io&grant_type=client_credentials'
+  headers = {
+    'Authorization': keys_bnb.refreshToken,
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  response = requests.request("POST", url, headers=headers, data = payload)
+  tokenResponseUtf8 = (response.text.encode('utf8'))
+  jsonNewToken = json.loads(tokenResponseUtf8)
+  newRefreshedToken = jsonNewToken['access_token']
+  try:  # Updating the authToken value in the resource file
+    with open('./secret/keys_bnb.py', "r+") as keyfile:
+      keyfileContent = keyfile.read()
+      keyfileContent = re.sub(
+          f'authToken=\"(.*?)\"', f'authToken=\"Bearer {newRefreshedToken}\"', keyfileContent)
+      keyfile.seek(0)
+      keyfile.write(keyfileContent)
+      keyfile.truncate()
+  except FileNotFoundError:
+    keyfileContent = None
+  importlib.reload(keys_bnb)  # reloading the updated authToken resource
+  print ("Access Token Has Been Refreshed")
 # print (getTodaysCheckins())
--- wink.py
+++ wink.py
@@ -4,32 +4,38 @@ from secret.urls_wink import baseURL
 from secret.keys_wink import authToken, contentType
 from bnb import getTodaysCheckins
 
-
-dummydata = [{'name': 'GuyCh', 'code': '1173', 'checkin': '2020-06-25',
-    'checkout': '2020-06-25', 'locks': ['257145']}]
-
 today = str(datetime.datetime.now()).split(" ")[0] #  'YYYY-MM-DD'
 
 def makeWorkOrder():
     TodaysCheckins = getTodaysCheckins()
-    if TodaysCheckins == None: # Happens sometimes after refreshing the bnb auth token
-      print ("Something didnt work. Trying again..")
-      TodaysCheckins = getTodaysCheckins()
-    print ("TodaysCheckins", TodaysCheckins)
-    workOrderDict = {}
-    workOrderList = []
-    for checkin in TodaysCheckins:
-        workOrderDict['name'] = checkin['name']
-        workOrderDict['code'] = checkin['code']
-        workOrderDict['checkin'] = checkin['checkin']
-        workOrderDict['checkout'] = checkin['checkout']
-        workOrderDict['locks'] = listinglocks[checkin['listing']]
-        workOrderList.append(workOrderDict)
-    return workOrderList
 
+    if TodaysCheckins == 404: # No guest activity today
+      workOrderList = 404
+      return workOrderList  
+    elif TodaysCheckins == None: # Happens sometimes after refreshing the bnb auth token
+        print ("Something didnt work. Trying again..")
+        TodaysCheckins = getTodaysCheckins('delete')
+        if TodaysCheckins == 404: # No guest activity today
+          workOrderList = 404
+          return workOrderList
+    else:
+        workOrderList = []
+        for checkin in TodaysCheckins:
+            workOrderDict = {}
+            workOrderDict['name'] = checkin['name']
+            workOrderDict['code'] = checkin['code']
+            workOrderDict['checkin'] = checkin['checkin']
+            workOrderDict['checkout'] = checkin['checkout']
+            workOrderDict['locks'] = listinglocks[checkin['listing']]
+            workOrderList.append(workOrderDict)
+        return workOrderList
 
 def programCodes(workorder):
-    for guest in workorder:
+  print ("Work Order: ",workorder)
+  if workorder==404:
+    print ("No guest activity today")
+  else:
+      for guest in workorder:
         locks = guest['locks']
         guestCode = guest['code']
         guestName = guest['name']
@@ -58,53 +64,54 @@ def programCodes(workorder):
         else: print ("No codes to program today")
 
 def deleteCodes(workorder):
-  for guest in workorder:
-    locks = guest['locks']
-    for lock in locks:
-      print (f"\n","Lock: ", lock)
-      print("------------------------------------------------------")
-      url = f"{baseURL}/locks/{lock}/keys"
-      guestName = guest['name']
-      guestCheckOut = guest['checkout']
-      payload = {}
-      headers = {
-        'Content-Type': contentType,
-        'Authorization': authToken
-        }
+  print ("Work Order: ",workorder)
+  if workorder==404:
+      print ("No guest activity today")
+  else:
+    for guest in workorder:
+      locks = guest['locks']
+      for lock in locks:
+        print (f"\n","Lock: ", lock)
+        print("------------------------------------------------------")
+        url = f"{baseURL}/locks/{lock}/keys"
+        guestName = guest['name']
+        guestCheckOut = guest['checkout']
+        payload = {}
+        headers = {
+          'Content-Type': contentType,
+          'Authorization': authToken
+          }
 
-      response = requests.request("GET", url, headers=headers, data = payload)
-      utf8Response = response.text.encode('utf8')
-      jsonResponse = json.loads(utf8Response)
-      keys = jsonResponse['data']
-      for key in keys:
-        if key['name'] == guestName and guestCheckOut==today:
-          print ("Boop Boop - Codes Are Being Deleted...")
-          try:
-            url = f"{baseURL}/keys/{key['key_id']}"
-            headers = {
-            'Content-Type': contentType,
-            'Authorization': authToken
-            }
+        response = requests.request("GET", url, headers=headers, data = payload)
+        utf8Response = response.text.encode('utf8')
+        jsonResponse = json.loads(utf8Response)
+        keys = jsonResponse['data']
+        for key in keys:
+          if key['name'] == guestName and guestCheckOut==today:
+            print ("Boop Boop - Codes Are Being Deleted...")
+            try:
+              url = f"{baseURL}/keys/{key['key_id']}"
+              headers = {
+              'Content-Type': contentType,
+              'Authorization': authToken
+              }
 
-            requests.request("DELETE", url, headers=headers, data = payload)
-            print("------------------------------------------------------")
-            print ("DELETE request for: ", guestName)
-            print ("Checking Out At: ", guestCheckOut)
-            print ("url: ", url)
-            print ("payload: ", payload)
-            print ("name: ",key['name'])
-            print ("key_id: ", key['key_id'])
-            print("------------------------------------------------------")
-            print("DONE")
+              requests.request("DELETE", url, headers=headers, data = payload)
+              print("------------------------------------------------------")
+              print ("DELETE request for: ", guestName)
+              print ("Checking Out At: ", guestCheckOut)
+              print ("url: ", url)
+              print ("payload: ", payload)
+              print ("name: ",key['name'])
+              print ("key_id: ", key['key_id'])
+              print("------------------------------------------------------")
+              print("DONE")
 
-          except Exception as e:
-            print(e)
-        else: print (key['name'], "Doesnt need to be deleted today")
-      
+            except Exception as e:
+              print(e)
+          else: print (key['name'], "Doesnt need to be deleted today")
+        
 
 # programCodes(makeWorkOrder())
 # deleteCodes(makeWorkOrder())
-
-# programCodes(dummydata)
-# deleteCodes(dummydata)
 
